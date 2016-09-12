82db5d42021dbbf5fbaf7b9c0ab8125bd35795f2
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-09-12 23:27:54 +0900

    change filename (URL) of canary and develop ver.

--- build/msvc/13-update-canary.bat
+++ build/msvc/13-update-canary.bat
@@ -6,8 +6,8 @@ SET REMOTE_HOST=koron@files.kaoriya.net
 SET REMOTE_WORKDIR=/home/htdocs/files.kaoriya.net/vim/
 SET RELZIP_W32=vim%VIM_VER_SHORT%-kaoriya-win32-%VIM_VER%-%PATCHSET_VER%.zip
 SET RELZIP_W64=vim%VIM_VER_SHORT%-kaoriya-win64-%VIM_VER%-%PATCHSET_VER%.zip
-SET ABSZIP_W32=vim%VIM_VER_SHORT%-kaoriya-win32-test.zip
-SET ABSZIP_W64=vim%VIM_VER_SHORT%-kaoriya-win64-test.zip
+SET ABSZIP_W32=vim-kaoriya-win32-canary.zip
+SET ABSZIP_W64=vim-kaoriya-win64-canary.zip
 
 ssh "%REMOTE_HOST%" "cd %REMOTE_WORKDIR%; ln -fv %RELZIP_W32% %ABSZIP_W32%; ln -fv %RELZIP_W64% %ABSZIP_W64%"
 

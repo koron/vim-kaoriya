0ea1161144c6237e3bd3c37dbeab841602899d25
MURAOKA Taro <koron.kaoriya@gmail.com>
2017-11-24 01:20:32 +0900

    use SDK 8.1 for missing headers

--- build/msvc/tools/msvc-nmake.bat
+++ build/msvc/tools/msvc-nmake.bat
@@ -1,4 +1,7 @@
 @ECHO OFF
 
+REM Use SDK 8.1 for missing headers.
+SET "INCLUDE=C:\Program Files (x86)\Windows Kits\8.1\include\shared;C:\Program Files (x86)\Windows Kits\8.1\include\um;C:\Program Files (x86)\Windows Kits\8.1\include\winrt"
+
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" %1
 nmake %2 %3 %4 %5 %6 %7 %8 %9

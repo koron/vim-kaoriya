7d7f910fc18fa38e4f16cc04fcbe19ac8745a806
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-07-18 18:13:57 +0900

    split upload scripts in two

--- build/msvc/20-upload-publish.bat
+++ build/msvc/20-upload-publish.bat
@@ -7,10 +7,5 @@ REM Uplaod release files to publishing server.
 ECHO Uploading...
 rsync -v target/%RELZIP_W32% target/%RELZIP_W64% target/%PDBZIP_W32% target/%PDBZIP_W64% "koron@files.kaoriya.net:htdocs/files.kaoriya.net/vim"
 
-REM Pool release files to pooling server.
-ECHO Pooling...
-SET YEAR=%date:~-10,4%
-rsync -v target/%RELZIP_W32% target/%RELZIP_W64% target/%PDBZIP_W32% target/%PDBZIP_W64% "koron@ixeris.kaoriya.net:pool/vim-jp/released/%YEAR%"
-
 PAUSE
 EXIT /B 0

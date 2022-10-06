5ad6957de2161a89b03506db2d5367eaabee7fd9
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-10-06 12:26:58 +0900

    catch up Vim 9.9.669

--- build/msvc/9A-daily-build.bat
+++ build/msvc/9A-daily-build.bat
@@ -12,12 +12,20 @@ REM Catcn up Vim's update
 git fetch -p
 git merge --ff-only @{u}
 
-REM TODO: update VIM_VER in ..\..\VERSION
+REM Update VIM_VER in ..\..\VERSION
+
+SET TEMP_PATCH_NUM=%TEMP%\%RANDOM%_vim_patch_num.txt
+sed -ne '/^static int included_patches/,/^\s*[0-9]\+,$/p' src/version.c | tail -1 | tr -cd 0123456789 > %TEMP_PATCH_NUM%
+SET /P PATCH_NUM=<%TEMP_PATCH_NUM%
+DEL /F %TEMP_PATCH_NUM%
+sed -i -e 's/^\(VIM_VER\s*=\s*[0-9]\+\.[0-9]\+\.\)[0-9]\+/\1%PATCH_NUM%/' ..\VERSION
 
 REM Apply patches with guilt
 %GUILTCMD% push -a
 
 CD "%CURDIR%"
+REM update VIM_VER
+CALL tools\command-common.bat
 
 REM Build Vim
 nmake /NOLOGO build-release

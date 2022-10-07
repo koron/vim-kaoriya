1fe72f2da9bd152a0604194e5b434d11e60fb265
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-10-07 11:43:17 +0900

    catch up Vim 9.0.0681

--- build/msvc/9A-daily-build.bat
+++ build/msvc/9A-daily-build.bat
@@ -15,7 +15,7 @@ git merge --ff-only @{u}
 REM Update VIM_VER in ..\..\VERSION
 
 SET TEMP_PATCH_NUM=%TEMP%\%RANDOM%_vim_patch_num.txt
-sed -ne '/^static int included_patches/,/^\s*[0-9]\+,$/p' src/version.c | tail -1 | tr -cd 0123456789 > %TEMP_PATCH_NUM%
+sed -ne '/^static int included_patches/,/^\s*[0-9]\+,$/p' src/version.c | tail -1 | tr -cd 0123456789 | xargs printf '%%04d' > %TEMP_PATCH_NUM%
 SET /P PATCH_NUM=<%TEMP_PATCH_NUM%
 DEL /F %TEMP_PATCH_NUM%
 sed -i -e 's/^\(VIM_VER\s*=\s*[0-9]\+\.[0-9]\+\.\)[0-9]\+/\1%PATCH_NUM%/' ..\VERSION

f85abc71414b7a4675f6499b01d9994168f4fa1c
MURAOKA Taro <koron.kaoriya@gmail.com>
2026-06-30 09:53:03 +0900

    Clean up build/msvc dir

--- build/msvc/tools/check.vim
+++
@@ -1,2 +0,0 @@
-call setline(1, 'version=' .. printf('%d.%d.%04d', v:version/100, v:version%100, v:versionlong%10000))
-call setline(2, 'kaoriya=' .. has('kaoriya'))
--- build/msvc/tools/msvc-nmake.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-REM Use SDK 8.1 for missing headers.
-REM SET "INCLUDE=C:\Program Files (x86)\Windows Kits\8.1\include\shared;C:\Program Files (x86)\Windows Kits\8.1\include\um;C:\Program Files (x86)\Windows Kits\8.1\include\winrt"
-
-REM CALL "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" %1
-CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %1
-nmake %2 %3 %4 %5 %6 %7 %8 %9
